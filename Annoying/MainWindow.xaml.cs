240a189d82f69ad1f0a477e4466c361529d3203b
Daron Yondem <daron@yondem.com>
2019-01-11 13:09:42 +0300

    Initial Implementation Complete

--- Annoying/MainWindow.xaml.cs
+++ Annoying/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +24,22 @@ namespace Annoying
         public MainWindow()
         {
             InitializeComponent();
+            btnSend.Click += BtnSend_ClickAsync;
+        }
+
+        private async void BtnSend_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var textMessage = txtMessage.Text;
+            var punctuation = textMessage.Where(Char.IsPunctuation).Distinct().ToArray();
+            var words = textMessage.Split().Select(x => x.Trim(punctuation));
+
+            await Task.Delay(3000);
+            foreach (var item in words)
+            {
+                SendKeys.SendWait(item);
+                SendKeys.SendWait("{Enter}");
+                await Task.Delay(1000);
+            }
         }
     }
 }
